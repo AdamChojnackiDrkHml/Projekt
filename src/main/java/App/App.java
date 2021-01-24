81ca6ac36079b51ddf5c820e7770a1fcd2843a30
Adam Chojnacki <adam.chojnacki.syl@gmail.com>
2021-01-24 21:55:31 +0100

    test bazy

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -11,19 +11,20 @@ public class App extends JFrame
 
 	public static void main(String[] args) throws SQLException
     {
-        /*DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+        DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
         String dbURL = "jdbc:sqlserver://localhost\\sqlexpress;user=Admin;password=dPl1s.mn]Q";
         Connection conn = DriverManager.getConnection(dbURL);
         if (conn != null) 
         {
             System.out.println("Connected");
         }
-        String statement = "SELECT * FROM Users";
+        String statement = "SELECT * FROM Courses";
         assert conn != null;
         Statement stm = conn.createStatement();
 
-        ResultSet resultSet = stm.executeQuery(statement);*/
- //       System.out.println(resultSet.getString(0));
+        ResultSet resultSet = stm.executeQuery(statement);
+        resultSet.next();
+        System.out.println(resultSet.getString("CourseName"));
         EventQueue.invokeLater(() -> new SemesterFrame());
     }
 }
