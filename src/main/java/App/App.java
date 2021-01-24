37ab7ea2d2b4a798974c89ae7ed64411f559bdb6
Ela kula daje fula <Ela kula daje fula@DESKTOP-T2V9KR1>
2021-01-24 21:53:08 +0100

    commented code

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -11,17 +11,18 @@ public class App extends JFrame
 
 	public static void main(String[] args) throws SQLException
     {
-        DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+        /*DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
         String dbURL = "jdbc:sqlserver://localhost\\sqlexpress;user=Admin;password=dPl1s.mn]Q";
         Connection conn = DriverManager.getConnection(dbURL);
-        if (conn != null) {
+        if (conn != null) 
+        {
             System.out.println("Connected");
         }
         String statement = "SELECT * FROM Users";
         assert conn != null;
         Statement stm = conn.createStatement();
 
-        ResultSet resultSet = stm.executeQuery(statement);
+        ResultSet resultSet = stm.executeQuery(statement);*/
  //       System.out.println(resultSet.getString(0));
         EventQueue.invokeLater(() -> new SemesterFrame());
     }
