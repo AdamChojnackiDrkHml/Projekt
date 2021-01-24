3b3e6d0925bddaa31b6104e064e26ae26c651dab
Adam Chojnacki <adam.chojnacki.syl@gmail.com>
2021-01-24 21:29:26 +0100

    test bazy

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -1,6 +1,7 @@
 package App;
 
 import java.awt.*;
+import java.sql.*;
 
 import javax.swing.*;
 
@@ -9,8 +10,19 @@ public class App extends JFrame
 	private static final long serialVersionUID = 1L;
 	
     //int numOfList = 0;
-	public static void main(String[] args)
+	public static void main(String[] args) throws SQLException
     {
+        DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+        String dbURL = "jdbc:sqlserver://localhost\\sqlexpress;user=Admin;password=dPl1s.mn]Q";
+        Connection conn = DriverManager.getConnection(dbURL);
+        if (conn != null) {
+            System.out.println("Connected");
+        }
+        String statement = "SELECT * FROM Users";
+        assert conn != null;
+        Statement stm = conn.createStatement();
+
+        ResultSet resultSet = stm.executeQuery(statement);
         EventQueue.invokeLater(() -> new SemesterFrame());
     }
 }
